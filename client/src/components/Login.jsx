2493da6ec6a224027ef06109a24d70e8210d0e3f
jagjeet147 <jagjeet695446@gmail.com>
2025-06-22 22:07:28 +0530

    log-ui-comp

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+
 import '../App.css'
 
 const Login = () => {
+
+
+
+    const handleLogin = async () => {
+        // Handle login logic here
+        // This function should send the email and password to your backend for authentication
+        // For example:
+        // await axios.post(URL + "/api/auth/login", { email, password }, { withCredentials: true });
+    }
+
     return (<>
-        <div class="Login">
-            <form class="Login-card">
-                <h1 class="Login-title">Login</h1>
+        <div className="Login">
+            <form onSubmit={handleLogin} className="Login-card">
+                <h1 className="Login-title">Login</h1>
 
-                <input class="Login-input" type="text" id="username" name="username" placeholder="Username"> </input>
-                <input class="Login-input" type="password" id="password" name="password" placeholder="Password"> </input>
-                <button class="Login-button" type="submit">Login</button>
-
+                <input className="Login-input" type="text" id="email" name="email" placeholder="Email"
+                // value={email}
+                // onChange={(e) => setEmail(e.target.value)}
+                />
+                <input className="Login-input" type="password" id="password" name="password" placeholder="Password"
+                // value={password}
+                // onChange={(e) => setPassword(e.target.value)}
+                />
+                <button className="Login-button" type="submit" onClick={handleLogin}>Login</button>
+                {/* {error && <h3 className="highlight">Something went wrong</h3>} */}
+                <p><span className="highlight"
+                //  onClick={() => navigate('/Sign')}
+                >Sign in </span> if you have not registered yet</p>
             </form>
+
+            <div className="google-login">
+                <img className="google-logo" src="https://img.icons8.com/?size=100&id=110560&format=png&color=000000"
+                    alt="Google Logo"></img>
+            </div>
         </div>
     </>
     )
--- client/src/components/Sign.jsx
+++ client/src/components/Sign.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
+
+import '../App.css'
 
 const Sign = () => {
+ 
+
+  const handleSignUp = async () => {
+    // Handle sign-up logic here
+    // This function should send the username, email, and password to your backend for registration
+    // For example:
+    // await axios.post(URL + "/api/auth/signup", { username, email, password }, { withCredentials: true });
+
+  }
+
   return (
-    <div class="Login">
-            <form class="Login-card">
-                <h1 class="Login-title">Sign Up</h1>
 
-                <input class="Login-input" type="text" id="username" name="username" placeholder="Username"> </input>
-                <input class="Login-input" type="password" id="password" name="password" placeholder="Password"> </input>
-                <button class="Login-button" type="submit">Sign Up</button>
+    <div className="Login">
+      <form onSubmit={handleSignUp} className="Login-card">
+        <h1 className="Login-title">Sign Up</h1>
 
-            </form>
-        </div>
+        <input className="Login-input" type="text" id="username" name="username" placeholder="Username" 
+        // value={username}
+          // onChange={(e) => setUsername(e.target.value)}
+          />
+        <input className="Login-input" type="text" id="email" name="email" placeholder="Email" 
+        // value={email}
+          // onChange={(e) => setEmail(e.target.value)}
+          />
+        <input className="Login-input" type="password" id="password" name="password" placeholder="Password" 
+        // value={password}
+          // onChange={(e) => setPassword(e.target.value)}
+          />
+        <button className="Login-button" type="submit" onClick={handleSignUp}>Sign Up</button>
+        {/* {error &&<h3 className="highlight">Something went wrong</h3>} */}
+        <p><span className="highlight"
+        //  onClick={() => navigate('/Login')}
+         >Login in </span> if you have already registered</p>
+
+      </form>
+
+      <div className="google-login">
+        <img className="google-logo" src="https://img.icons8.com/?size=100&id=110560&format=png&color=000000"
+          alt="Google Logo"></img>
+      </div>
+    </div>
   )
 }
 
