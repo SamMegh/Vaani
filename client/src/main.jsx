1426c7017ddb611b62e310dc831398fcda014f81
jagjeet147 <jagjeet695446@gmail.com>
2025-06-23 10:54:29 +0530

    loginUI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Login from './components/Login.jsx'
+import Sign from './components/Sign.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Login />
+    <Sign />
   </StrictMode>,
 )
