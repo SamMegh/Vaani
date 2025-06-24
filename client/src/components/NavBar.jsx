a66e69c0e9080d74d68962787369bd4ab9e972cc
jagjeet147 <jagjeet695446@gmail.com>
2025-06-24 17:20:55 +0530

    nav complite ui

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,30 +1,39 @@
 import React from 'react'
 
 const NavBar = () => {
-  return (
-    <div>
-         <nav className="navbar">
-        <div className="navbar-title">
-            <img className="navbar-logo" src="../images2/dragon_1.jpg" alt="Logo" />
-        </div>
-        <div className="navbar-list">
-            <div className="item">
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="navbar-title">
+                    <img className="navbar-logo" src="../src/assets/images2/dragon_7.jpg" alt="Site Logo" />
+                </div>
+                <div className="navbar-list">
+                    <div className="item">
+                        <div className="navbar-item">
 
-                <div className="navbar-item margin">H</div>
-                <div className="navbar-item margin">A</div>
-                <div className="navbar-item margin">C</div>
-                <div className="navbar-item margin">C</div>
-                <div className="navbar-item margin">C</div>
-            </div>
-            <div className="sating">
-                <div className="navbar-item margin">o</div>
-                <img className="user-logo " src="../images2/dragon_1.jpg" alt="Logo" />
-            </div>
-        </div>
-    </nav>
+                        </div>
+                        <div className="navbar-item">
 
-    </div>
-  )
+                        </div>
+                        <div className="navbar-item">
+
+                        </div>
+                        <div className="navbar-item">
+
+                        </div>
+                        <div className="navbar-item">
+
+                        </div>
+                    </div>
+                    <div className="sating">
+                        <div className="navbar-item">O</div>
+                        <img className="user-logo" src="../src/assets/images2/dragon_3.jpg" alt="User Logo" />
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+    )
 }
 
 export default NavBar
