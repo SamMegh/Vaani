126209117ea997dfce1e2bb8c918340c31187d6a
jagjeet147 <jagjeet695446@gmail.com>
2025-06-24 16:35:57 +0530

    js-font file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={isAuthuser ? <HomeScreen /> : <Navigate to="/login" />}
+            element={!isAuthuser ? <HomeScreen /> : <Navigate to="/login" />}
           />
           <Route
             path="/login"
