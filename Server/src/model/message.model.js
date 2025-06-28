77b14684411bd0a05e13a279d41968c0f24ca0ff
Ankit <sam.megh0305@gmail.com>
2025-06-28 23:48:46 +0530

    message model is ready

---
+++ Server/src/model/message.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    senderid: {
+        type: String,
+        required: true,
+        ref: "User"
+    },
+    receiverid: {
+        type: String,
+        required: true,
+        ref: "User"
+    },
+    name: {
+        type: String,
+        ref: "User",
+        required: true
+    },
+    message: {
+        type: String,
+    },
+}, {
+    timestamps: true
+});
+
+const Message= mongoose.model("Message",messageSchema)
+
+export default Message
