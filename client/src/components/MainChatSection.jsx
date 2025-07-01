bcd5dd7add09a3fc6f157509a6b5f175fd86f145
Ankit <sam.megh0305@gmail.com>
2025-07-01 18:01:11 +0530

    error on signout is solved

--- client/src/components/MainChatSection.jsx
+++ client/src/components/MainChatSection.jsx
@@ -3,15 +3,12 @@ import { useChatStore } from "../store/useChatStore.jsx";
 import { useAuthStore } from "../store/useAuthStore.jsx";
 import { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPlus,
-  faWaveSquare,
-} from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faWaveSquare } from "@fortawesome/free-solid-svg-icons";
 
 const MainChatSection = () => {
-  const { messages, sendMessage, getMessage, setCurrentRoom  } = useChatStore();
+  const { messages, sendMessage, getMessage, setCurrentRoom } = useChatStore();
   const { isAuthuser } = useAuthStore();
-  const myId = isAuthuser.uid;
+  const myId = isAuthuser._id;
   const [msg, setMsg] = useState("");
   const handleSend = () => {
     if (msg.trim) {
@@ -20,55 +17,60 @@ const MainChatSection = () => {
     }
   };
   useEffect(() => {
-    getMessage();
     // realTimeConvertiate();
-      if (messageBodyRef.current) {
+    if (messageBodyRef.current) {
       messageBodyRef.current.scrollTop = messageBodyRef.current.scrollHeight;
     }
     // return()=>deleteRealTimeConvertiate();
-  }, [getMessage, setCurrentRoom, messages])
+  }, [setCurrentRoom, messages]);
 
   const messageBodyRef = useRef(null);
 
   return (
-    <div className='chat-main'>
+    <div className="chat-main">
       <div className="message-body " ref={messageBodyRef}>
         <h1 className="message-title">list of messages are </h1>
         {messages.map((msg, index) => (
-          <li className={msg.senderId == myId ? "itsMeClass" : (msg.senderId == "AssitantReplyGroq") ? "assistantclass" : "otherUserClass"}
-            key={msg.id || index}
+          <li
+            className={
+              msg.senderid === myId
+                ? "itsMeClass"
+                : msg.senderid === "AssitantReplyGroq"
+                ? "assistantclass"
+                : "otherUserClass"
+            }
+            key={msg._id || index}
           >
-            <span className="msg-rol " >
-              {msg.rool}
-            </span>
-            <span className="msg-prompt " >
-              {msg.prompt}
-            </span>
+            <span className="msg-rol">{msg.name}</span>
+            <span className="msg-prompt">{msg.message
+  }</span>
           </li>
         ))}
       </div>
       <div className="message-input">
-
-
         <div className="chat-input-wrapper wrapper">
-          <FontAwesomeIcon icon={faPlus} className="fas fa-plus styles-icon" />
-          <textarea type="text" placeholder="Ask anything" value={msg}
-            onChange={(e) => setMsg(e.target.value)} className="styles-input"
-            rows={2} />
-          <FontAwesomeIcon icon={faWaveSquare} className="fas fa-wave-square styles-icon" onClick={handleSend} />
+          <FontAwesomeIcon
+            icon={faPlus}
+            className="fas fa-plus styles-icon"
+            onClick={getMessage}
+          />
+          <textarea
+            type="text"
+            placeholder="Ask anything"
+            value={msg}
+            onChange={(e) => setMsg(e.target.value)}
+            className="styles-input"
+            rows={2}
+          />
+          <FontAwesomeIcon
+            icon={faWaveSquare}
+            className="fas fa-wave-square styles-icon"
+            onClick={handleSend}
+          />
         </div>
-
-
       </div>
-
-
-
-
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default MainChatSection
+export default MainChatSection;
